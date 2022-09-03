198480baba683c0d7d3e25fe9ddd78fb7707f140
brucab <cabrerabru@gmail.com>
2022-09-02 21:21:19 -0300

    Strings

---
+++ Intro/Strings.py
@@ -0,0 +1,18 @@
+print("today is a good day to learn python")
+
+# si quiero usar un ', si o si tengo que usar ""
+print("Bruno's score is 5")
+
+# cocnatenate
+
+print("Hola" + " Bruno")
+
+greeting = "Hello"
+person = "Bruno"
+
+print(greeting + ' ' + person)
+
+# con intervencion
+greeting = "Hello"
+user = input("Please enter your name: ")
+print(greeting + ' ' + user)
