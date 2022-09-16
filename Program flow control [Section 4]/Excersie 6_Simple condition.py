7a306d2cbc6b367f5b531aa61bd9384389002915
brucab <cabrerabru@gmail.com>
2022-09-16 19:56:00 -0300

    Star section 4

---
+++ Program flow control [Section 4]/Excersie 6_Simple condition.py
@@ -0,0 +1,10 @@
+x = 5
+y = 7
+
+if x > y:
+    print("{0} is greater than {1}".format(x, y))
+elif x < y:
+    print("{0} is smaller than {1}".format(x, y))
+else:
+    print("{0} equals {1}".format(x, y))
+
