ddbf0175603acddb1075db0a1b655dbc29f4d5e6
brucab <cabrerabru@gmail.com>
2022-09-18 20:20:00 -0300

    More on fors

---
+++ Program flow control [Section 4]/Exercise 7_extracting capitals.py
@@ -0,0 +1,8 @@
+quote = "Alright, but apart from the Sanitation, the Medicine, Education, Wine, Public Order, Irrigation, Roads, " \
+        "the Fresh-Water System, and Public Health, what have the Romans ever done for us? "
+upper = ""
+
+for cha in quote:
+    if cha.isupper():
+        upper = upper + cha
+print(upper)
